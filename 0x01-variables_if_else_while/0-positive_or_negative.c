8487b8828675d67ea14a2cb511e32769a0567c92
sadikurilizer5050 <you@example.com>
2022-09-11 02:38:46 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,41 +5,31 @@
 #include <time.h>
 
 /**
- * main - Prints a ransom number and states whether
- * it is positive, negative, or zero
- * Return 0;
- */
-
-
-
-/* betty style doc for function main goes there */
-
+* Main - Prints a ransom number and states whether
+* it is positive, negative, or zero
+*
+* Return: 0,
+*/
+/* Main- the code start in main*/
 int main(void)
 
-{
-
-		int n;
-
-
-
-			srand(time(0));
+{ int n;
+		srand(time(0));
 
 				n = rand() - RAND_MAX / 2;
 
 					/* your code goes there */
-if  (n > 0){ 
-	printf("%d",n);
-	printf(" is positive\n");
+if (n > 0)
+{printf("%d", n);
+printf(" is positive\n");
 }
-
-else if (n<0){ printf("%d",n);
-	printf(" is negative\n");
+else if (n < 0)
+{printf("%d", n);
+printf(" is negative\n");
 }
-
-else {printf("%d",n);
-	 printf(" is zero\n");
+else
+{printf("%d", n);
+printf(" is zero\n");
 }
-
 return (0);
-
 }
