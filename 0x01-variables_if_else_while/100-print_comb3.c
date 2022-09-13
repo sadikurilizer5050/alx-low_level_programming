acf5b7d65948149fa26aa67d5a5b5dab080e5b7c
sadikurilizer5050 <you@example.com>
2022-09-12 17:09:27 -0700

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,15 +7,23 @@
 */
 
 int main(void)
-{ int num = 0;
-	for (num = 0; num < 90; num++)
-	{ putchar((num / 10) + '0');
-	putchar ((num % 10) + '0');
-		if (num != 99)
-		{putchar(',');
-		putchar(' ');
+{int d;
+int p;
+for (d = 0; d <= 9; d++)
+{
+	for (p = 0; p <= 9; p++)
+	{
+		if (d < p)
+		{putchar(d + '0');
+		putchar(p + '0');
+			if (d != 8 || (d == 8 && p != 9))
+			{
+			putchar(',');
+			putchar(' ');
+			}
 		}
 	}
+}
 putchar('\n');
 return (0);
 }
---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - a program that prints all possible
+* different combinations of three digits.
+*
+* Return: 0;
+*/
+
+int main(void)
+{ int d;
+
+}
