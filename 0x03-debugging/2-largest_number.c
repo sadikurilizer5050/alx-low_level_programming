52cd831a168c49a05539d895c6875651ecc80de0
sadikurilizer5050 <you@example.com>
2022-09-16 19:50:28 -0700

    Task 2. 0 > 972?

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* largest_number – returns the largest of three numbers
+* largest_number - returns the largest of three numbers
 * @a: first integer
 * @b: second integer
 * @c: third integer
