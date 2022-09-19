c129b1066efc88eed7f6f4476e6fd224c15a92ef
sadikurilizer5050 <you@example.com>
2022-09-19 07:15:14 -0700

    8-print_array.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,6 +7,7 @@ void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 size_t _strlen(const char *str);
 void _puts(char *str);
+void print_array(int *a, int n);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
